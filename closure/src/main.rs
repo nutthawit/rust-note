ac4c59e1d94e2f355903e548d018d5ce6e445722
kdtie <nutthawit1513@gmail.com>
2025-08-22 08:17:07 +0700

    add closure

---
+++ closure/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let haystack = vec![1, 2, 3];
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&2));
+
+    // println!("{:?}", haystack);
+}
